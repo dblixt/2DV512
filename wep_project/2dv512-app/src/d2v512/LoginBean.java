18d8e24f95921529973722b61fdc809f511d76ee
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2015-11-21 11:42:06 +0100

    project shell

---
+++ wep_project/2dv512-app/src/d2v512/LoginBean.java
@@ -0,0 +1,33 @@
+package d2v512;
+
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class LoginBean implements Serializable {
+	private static final long serialVersionUID = 1610404137333266630L;
+	
+	private String email;
+	private String password;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+}
