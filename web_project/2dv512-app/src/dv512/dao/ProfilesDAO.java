2e5086709bafde2be5d94004101b8c55c76ef7c4
Marten Isaksson <morten.isaksson@gmail.com>
2015-12-07 23:37:06 +0100

    Update
    Update

--- web_project/2dv512-app/src/dv512/dao/ProfilesDAO.java
+++ web_project/2dv512-app/src/dv512/dao/ProfilesDAO.java
@@ -5,26 +5,23 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import dv512.controller.util.DbManager;
-import dv512.model.Dog;
 import dv512.model.Profile;
 
 @Named
-@SessionScoped
+@ApplicationScoped
 public class ProfilesDAO implements Serializable {
 
 	private static final long serialVersionUID = 5568131005011243224L;
-
+	
 	@Inject
 	private DbManager dbManager;
-
+		
 	public Profile get(int userId) {
 		Connection con = null;
 		PreparedStatement stmt = null;
@@ -32,38 +29,39 @@ public class ProfilesDAO implements Serializable {
 			con = dbManager.getConnection();
 			stmt = con.prepareStatement("SELECT * FROM Profiles WHERE user_id = ?");
 			stmt.setInt(1, userId);
-
+		
 			ResultSet r = stmt.executeQuery();
-
+			
 			Profile profile = new Profile();
-			if (r != null && r.next()) {
+			if(r != null && r.next()) {
 				profile.setUserId(r.getInt("user_id"));
 				profile.setName(r.getString("name"));
 				profile.setGender(r.getString("gender"));
 				profile.setDescription(r.getString("description"));
 				profile.setLatitude(r.getDouble("pos_lat"));
 				profile.setLongitude(r.getDouble("pos_lng"));
-				profile.setProfilePic(r.getString("img"));
-			}
-
+				profile.setProfilePic(r.getString("img"));	
+			}		
+			
 			return profile;
-		} catch (SQLException e) {
+		}
+		catch(SQLException e) {
 			e.printStackTrace();
-		} finally {
+		}
+		finally {
 			dbManager.close(stmt);
 			dbManager.close(con);
-		}
-
+		}	
+		
 		return null;
 	}
-
+	
 	public boolean update(Profile profile) {
 		Connection con = null;
 		PreparedStatement stmt = null;
 		try {
 			con = dbManager.getConnection();
-			stmt = con.prepareStatement(
-					"UPDATE Profiles SET name = ?, gender = ?, description = ?, img = ?, pos_lng = ?, pos_lat = ? WHERE user_id = ?");
+			stmt = con.prepareStatement("UPDATE Profiles SET name = ?, gender = ?, description = ?, img = ?, pos_lng = ?, pos_lat = ? WHERE user_id = ?");
 			stmt.setString(1, profile.getName());
 			stmt.setString(2, profile.getGender());
 			stmt.setString(3, profile.getDescription());
@@ -71,44 +69,51 @@ public class ProfilesDAO implements Serializable {
 			stmt.setDouble(5, profile.getLongitude());
 			stmt.setDouble(6, profile.getLatitude());
 			stmt.setInt(7, profile.getUserId());
-
-			stmt.executeUpdate();
+							
+			stmt.executeUpdate();	
 			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
+		}
+		catch(SQLException e) {
+			e.printStackTrace();			
+		}
+		finally {
 			dbManager.close(stmt);
 			dbManager.close(con);
-		}
-
+		}		
+		
 		return false;
 	}
-
-	public List<Profile> listAllEvent(int eventID) {
-		List<Profile> pl = new ArrayList<>();
-
+	
+	public boolean insert(Profile profile){
 		Connection con = null;
 		PreparedStatement stmt = null;
 		try {
 			con = dbManager.getConnection();
-			stmt = con.prepareStatement("SELECT * FROM EventJoins WHERE event_id = ?");
-			stmt.setInt(1, eventID);
-
-			ResultSet r = stmt.executeQuery();
-
-			while (r.next()) {
-				Profile p = get(r.getInt("user_id"));
-				pl.add(p);
-			}
 			
-		} catch (SQLException e) {
+			stmt = con.prepareStatement("INSERT INTO Profiles(user_id,name,gender,description,pos_lat,pos_lng,img) VALUES(?,?,?,?,?,?,?)");
+			stmt.setInt(1, profile.getUserId());
+			stmt.setString(2, profile.getName());
+			stmt.setString(3, null);
+			stmt.setString(4, null);
+			stmt.setString(5, null);
+			stmt.setString(6, null);
+			stmt.setString(7, null);
+			
+			stmt.executeUpdate();
+								
+		}
+		catch(SQLException e) {
 			e.printStackTrace();
-		} finally {
+			return false;
+		}
+		finally {
 			dbManager.close(stmt);
 			dbManager.close(con);
-		}
-
-		return pl;
+		}	
+		
+		return true;
 	}
-
+	
+	
+	
 }
