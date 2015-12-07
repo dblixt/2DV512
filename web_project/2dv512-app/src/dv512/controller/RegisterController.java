17fc0a439a69a2c1507ad54601c70698af4de98f
dblixt <blixt_89@hotmail.com>
2015-12-07 13:25:40 +0100

    created login/registration page

--- web_project/2dv512-app/src/dv512/controller/RegisterController.java
+++ web_project/2dv512-app/src/dv512/controller/RegisterController.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -15,18 +16,24 @@ import dv512.controller.util.DbManager;
 
 @Named
 @SessionScoped
-public class RegisterController implements Serializable{
+public class RegisterController implements Serializable {
 
 	private static final long serialVersionUID = -5690292807686490605L;
-	
+
 	public static final String ACTION_REGISTER_SUCCESS = "success";
 	public static final String ACTION_REGISTER_FAIL = "fail";
+
+	private final int DEFAULT_MODE = 0;
+	private final int REGISTER_MODE = 1;
+	private final int SUCESS_MODE = 2;
+	private final int FAILED_MODE = 3;
+
+	private int mode = DEFAULT_MODE;
 
 	private String name;
 	private String password;
 	private String email;
-	
-	
+
 	@Inject
 	private DbManager dbManager;
 
@@ -42,6 +49,10 @@ public class RegisterController implements Serializable{
 		this.email = email;
 	}
 
+	public void setMode(int mode) {
+		this.mode = mode;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -54,43 +65,59 @@ public class RegisterController implements Serializable{
 		return email;
 	}
 
+	public int getMode() {
+		return mode;
+	}
+
+	public void switchMode(AjaxBehaviorEvent event) {
+		// System.out.println("Inside switchMode");
+		if (mode == DEFAULT_MODE) {
+			mode = REGISTER_MODE;
+			System.out.println("Mode = " + mode);
+			return;
+		}
+		if (mode == REGISTER_MODE) {
+			mode = DEFAULT_MODE;
+			System.out.println("Mode = " + mode);
+		}
+	}
+
 	public String register() {
 		Connection con = null;
 		PreparedStatement s = null;
-		
-		try {			
+
+		try {
 			con = dbManager.getConnection();
-			
+
 			s = con.prepareStatement("INSERT INTO Users(email, password) VALUES(?,?)", Statement.RETURN_GENERATED_KEYS);
-			
+
 			s.setString(1, email);
 			s.setString(2, password);
-			s.executeUpdate();			
-			
+			s.executeUpdate();
+
 			int userId = 0;
 			// retrieve auto generated user id.
 			ResultSet key = s.getGeneratedKeys();
-			if(key.next()) {
+			if (key.next()) {
 				userId = key.getInt("id");
 			}
-						
+
 			dbManager.close(s);
-						
+
 			s = con.prepareStatement("INSERT INTO Profiles(user_id,name) VALUES(?,?)");
 			s.setInt(1, userId);
 			s.setString(2, name);
-			
-			s.executeUpdate();		
-		} 
-		catch (SQLException e) {
+
+			s.executeUpdate();
+		} catch (SQLException e) {
 			e.printStackTrace();
+			mode = FAILED_MODE;
 			return ACTION_REGISTER_FAIL;
-		}
-		finally {
+		} finally {
 			dbManager.close(con);
 			dbManager.close(s);
 		}
-		
+		mode = SUCESS_MODE;
 		return ACTION_REGISTER_SUCCESS;
 	}
 
