c29b0f74681f5e154754ab4c80320037ff3a9796
Marten Isaksson <morten.isaksson@gmail.com>
2015-12-07 14:31:55 +0100

    Updated Tabeles

---
+++ web_project/2dv512-app/src/dv512/model/Comment.java
@@ -0,0 +1,15 @@
+package dv512.model;
+
+public class Comment {
+	
+	private int commentId;
+	private int eventId;
+	private int userID;
+	private String comment;
+	private long dateTime;
+	
+	
+	
+	
+
+}
