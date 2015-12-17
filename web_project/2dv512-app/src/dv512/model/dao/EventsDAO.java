9fd66bcaa3d992b3ca929ee861b8beab627e5764
dblixt <blixt_89@hotmail.com>
2015-12-17 13:19:57 +0100

    fixed conflict

--- web_project/2dv512-app/src/dv512/model/dao/EventsDAO.java
+++ web_project/2dv512-app/src/dv512/model/dao/EventsDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,21 +27,18 @@ public class EventsDAO implements Serializable {
 	@Inject
 	private DbManager dbManager;
 
-	public Event get(int eventId) {
-
+	public Event get(int eventId) {		
 		Connection con = null;
 		PreparedStatement stmt = null;
 		try {
 			con = dbManager.getConnection();
-
 			stmt = con.prepareStatement("SELECT * FROM Events LEFT JOIN Profiles ON Events.user_id = Profiles.user_id WHERE id = ?");
-
 			stmt.setInt(1, eventId);
 
 			ResultSet r = stmt.executeQuery();
 
 			Event event = new Event();
-			if (r != null && r.next()) {
+			if (r.next()) {
 				
 				Profile profile = new Profile();		
 				profile.setUserId(r.getInt("user_id"));
@@ -53,7 +51,7 @@ public class EventsDAO implements Serializable {
 				
 				event.setId(r.getInt("id"));
 				event.setCreator(profile);
-				event.setDate(r.getLong("date"));
+				event.setDate(r.getLong("utc_date"));
 				event.setTitle(r.getString("title"));
 				event.setDescription(r.getString("description"));
 				event.setLongitude(r.getDouble("pos_lng"));
@@ -72,26 +70,27 @@ public class EventsDAO implements Serializable {
 		return null;
 	}
 
-
-	public boolean insert(Event event) {
-		
+	public boolean insert(Event event) {		
 		Connection con = null;
 		PreparedStatement stmt = null;
 		try {
 			con = dbManager.getConnection();
 			stmt = con.prepareStatement(
-					"INSERT INTO Events(user_id, date, title, description, pos_lng, pos_lat) VALUES(?,?,?,?,?,?)");
+					"INSERT INTO Events(user_id, utc_date, title, description, pos_lng, pos_lat, utc_date_modified) VALUES(?,?,?,?,?,?,?)");
 			stmt.setInt(1, event.getCreator().getUserId());
 			stmt.setLong(2, event.getDate());
 			stmt.setString(3, event.getTitle());
 			stmt.setString(4, event.getDescription());
 			stmt.setDouble(5, event.getLongitude());
 			stmt.setDouble(6, event.getLatitude());
+			stmt.setLong(7,  Instant.now().getEpochSecond());
 			stmt.executeUpdate();
 			return true;
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
+		} 
+		finally {
 			dbManager.close(stmt);
 			dbManager.close(con);
 		}
@@ -99,34 +98,40 @@ public class EventsDAO implements Serializable {
 		return false;
 	}
 
-	public void createTestEvent() {
-
-		Event event = new Event();
-
-		event.setId(1);
-		event.setDate(System.currentTimeMillis());
-		event.setTitle("Wark in the park");
-		event.setDescription(
-				"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.");
-		event.setLatitude(56.8483223);
-		event.setLatitude(14.8193872);
-		insert(event);
-
-		eventJoin(1, 1, System.currentTimeMillis());
-
-	}
-
-	public boolean eventJoin(int userId, int eventId, long date) {
-
+	
+	public boolean join(int userId, int eventId, long date) {
 		Connection con = null;
 		PreparedStatement stmt = null;
 		try {
 			con = dbManager.getConnection();
 			stmt = con.prepareStatement(
-					"INSERT INTO EventJoins(user_id, event_id, date) VALUES(?,?,?)");
+					"INSERT INTO EventJoins(user_id, event_id, utc_date) VALUES(?,?,?)");
 			stmt.setInt(1, userId);
 			stmt.setInt(2, eventId);
 			stmt.setLong(3, date);
+			stmt.executeUpdate();
+			return true;
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			dbManager.close(stmt);
+			dbManager.close(con);
+		}
+
+		return false;
+
+	}
+	
+	public boolean leave(int userId, int eventId) {
+		Connection con = null;
+		PreparedStatement stmt = null;
+		try {
+			con = dbManager.getConnection();
+			stmt = con.prepareStatement("DELETE FROM EventJoins WHERE user_id = ? AND event_id = ?");
+			stmt.setInt(1, userId);
+			stmt.setInt(2, eventId);
 			stmt.executeUpdate();
 			return true;
 		} catch (SQLException e) {
@@ -137,10 +142,9 @@ public class EventsDAO implements Serializable {
 		}
 
 		return false;
-
 	}
 
-
+	
 	public List<Event> feed(int userId, LatLng origin, double radius) {
 		List<Event> feed = new ArrayList<>();
 			
@@ -154,15 +158,29 @@ public class EventsDAO implements Serializable {
 			while(r.next()) {
 				Event e = new Event();
 				e.setId(r.getInt("id"));
-				e.getCreator().setId(r.getInt("user_id"));
+				e.getCreator().setUserId(r.getInt("user_id"));
 				e.getCreator().setName(r.getString("creator_name"));
 				e.getCreator().setImage(r.getString("creator_img"));
-				e.setDate(r.getLong("date"));
+				e.setDate(r.getLong("utc_date"));
 				e.setTitle(r.getString("title"));
 				e.setDescription(r.getString("description"));
 				e.setLatitude(r.getDouble("pos_lat"));
 				e.setLongitude(r.getDouble("pos_lng"));
-				e.setJoined(r.getInt("joined") > 0);
+				
+				int joined = r.getInt("joined");
+				int approved = r.getInt("join_approved");				
+				if(joined > 0) {
+					if(approved > 0) {
+						e.setJoinStatus(Event.JOIN_STATUS_JOINED);
+					}
+					else {
+						e.setJoinStatus(Event.JOIN_STATUS_JOIN_REQUESTED);
+					}
+				}
+				else {
+					e.setJoinStatus(Event.JOIN_STATUS_UNJOINED);
+				}
+
 				e.setDistance(r.getDouble("distance"));		
 				feed.add(e);
 			}		
@@ -181,54 +199,24 @@ public class EventsDAO implements Serializable {
 	private PreparedStatement createFeedStatement(Connection con, 
 			int userId, LatLng origin, double radius) throws SQLException {
 //		-------------------------------------------
-// 		Efficiency: not sure, given index on 
+// 		Efficiency: not sure, given indices on 
 //		-------------------------------------------
 //		Events.user_id
 //		Events.pos_lat
 //		Events.pos_lng
-//		Events.date
+//		Events.utc_date
+//		Events.utc_date_modified
 //		EventJoins.user_id
 //		EventJoins.event_id
 //		-------------------------------------------
-//		I don't think it would be too bad since the first selection
-//		is limited to 100 rows.
-//		
-//		
-//		SELECT s.id, s.user_id, u.name AS creator_name, u.img AS creator_img, s.date, 
-//		s.title, s.description, s.pos_lat, s.pos_lng, j.user_id AS joined, distance, radius FROM (
-//			SELECT e.id, e.user_id, title, description, 
-//			date, e.pos_lat, e.pos_lng, p.radius, 
-//			p.distance_unit 
-//				* DEGREES(ACOS(COS(RADIANS(p.latpoint)) 
-//				* COS(RADIANS(e.pos_lat)) 
-//				* COS(RADIANS(p.longpoint - e.pos_lng)) 
-//				+ SIN(RADIANS(p.latpoint)) 
-//				* SIN(RADIANS(e.pos_lat)))) AS distance 
-//			FROM Events AS e 
-//			JOIN (
-//				SELECT 56.8 AS latpoint, 14.8 AS longpoint, 
-//				50.0 AS radius, 111.045 AS distance_unit 
-//				FROM sysibm.sysdummy1 
-//			) AS p ON 1=1 
-//			WHERE e.pos_lat 
-//				BETWEEN p.latpoint  - (p.radius / p.distance_unit) 
-//				AND p.latpoint + (p.radius / p.distance_unit) 
-//			AND e.pos_lng 
-//				BETWEEN p.longpoint - (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint)))) 
-//				AND p.longpoint + (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint)))) 
-//			ORDER BY date
-//			FETCH FIRST 100 ROWS ONLY
-//		) AS s 
-//		LEFT JOIN Profiles AS u ON u.user_id = s.user_id 
-//		LEFT JOIN EventJoins AS j ON j.event_id = s.id AND j.user_id = userId 
-//		WHERE distance <= radius
-//
+//		it might be ok.
 	
 		String sql = 
-				"SELECT s.id, s.user_id, u.name AS creator_name, u.img AS creator_img, s.date, " + 
-				"s.title, s.description, s.pos_lat, s.pos_lng, j.user_id AS joined, distance, radius FROM ( " + 
+				"SELECT s.id, s.user_id, u.name AS creator_name, u.img AS creator_img, s.utc_date, " + 
+				"s.title, s.description, s.pos_lat, s.pos_lng, j.user_id AS joined, " +
+				"j.approved AS join_approved, distance, s.utc_date_modified, radius FROM ( " + 
 				"	SELECT e.id, e.user_id, title, description, " + 
-				"	date, e.pos_lat, e.pos_lng, p.radius, " + 
+				"	utc_date, e.pos_lat, e.pos_lng, e.utc_date_modified, p.radius, " + 
 				"	p.distance_unit " + 
 				"		* DEGREES(ACOS(COS(RADIANS(p.latpoint)) " + 
 				"		* COS(RADIANS(e.pos_lat)) " + 
@@ -248,16 +236,18 @@ public class EventsDAO implements Serializable {
 				"	AND e.pos_lng " + 
 				"		BETWEEN p.longpoint - (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint)))) " + 
 				"		AND p.longpoint + (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint)))) " + 
-				"	ORDER BY date " + 
-				"	FETCH FIRST 100 ROWS ONLY " + 
 				") AS s " + 
 				"LEFT JOIN Profiles AS u ON u.user_id = s.user_id " + 
 				"LEFT JOIN EventJoins AS j ON j.event_id = s.id AND j.user_id = ? " +
-				"WHERE distance <= radius";
+				"WHERE distance <= radius " +
+				"ORDER BY utc_date_modified DESC " + 
+				"FETCH FIRST 100 ROWS ONLY";
 				
 		PreparedStatement stmt = con.prepareStatement(sql);
 		stmt.setInt(1, userId);
 		return stmt;
 	}
 	
+}
+
 }
