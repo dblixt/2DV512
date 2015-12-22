8c621ded055459c4e7fb27e887805f18e747179a
dblixt <blixt_89@hotmail.com>
2015-12-22 12:12:26 +0100

    Event related updates
    Fixed event css so it works with Chrome browser
    Fixed event location showing properly on map when editing and creating
    EditEventController updated to use the correct controllers.
    Fixed update functionality in EventDAO

--- web_project/2dv512-app/src/dv512/controller/EditEventController.java
+++ web_project/2dv512-app/src/dv512/controller/EditEventController.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import dv512.model.Event;
-import dv512.model.Profile;
 import dv512.model.dao.EventsDAO;
 
 @Named
@@ -16,20 +15,22 @@ public class EditEventController implements Serializable {
 	private static final long serialVersionUID = 6667656806561372380L;
 	private final String CREATE_SUCESS = "success";
 	private final String CREATE_FAILED = "fail";
-	
+
 	@Inject
 	private EventsDAO eventDAO;
+
 	@Inject
-	private LoginController loginController;
-	
+	private UserController userController;
+
 	private Event event;
-	
-	/*id of event if in edit mode*/
+
+	/* id of event if in edit mode */
 	private int editEventId = -1;
-	
+
 	public Event getEvent() {
 		return event;
 	}
+
 	public int getEditEventId() {
 		return editEventId;
 	}
@@ -40,32 +41,34 @@ public class EditEventController implements Serializable {
 
 	public String createEvent() {
 		System.out.println("Creating event");
-		Profile p = new Profile();
-		p.setUserId(loginController.getUserId());
-		event.setCreator(p);
-		System.out.println(event.getCreator().getUserId());	
-		try{
-		eventDAO.insert(event);
-		System.out.println("Event created");
-		return CREATE_SUCESS;
-		}catch(Exception e) {
+		event.setCreator(userController.getProfile());
+		try {
+			eventDAO.insert(event);
+			System.out.println("Event created");
+			return CREATE_SUCESS;
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Failed to create event");
 			return CREATE_FAILED;
 		}
 	}
-	
-	public void loadEvent(){
-		if(event == null){
-			if(editEventId == -1) {
+
+	public void loadEvent() {
+		if (event == null) {
+			if (editEventId == -1) {
 				event = new Event();
+				event.setLatitude(userController.getProfile().getLatitude());
+				event.setLongitude(userController.getProfile().getLongitude());
 				return;
 			}
 			event = eventDAO.get(editEventId);
 		}
-		}
-		
-
+	}
 	
-}
+	public void editEvent(){
+		if(event != null) {
+			eventDAO.update(event);
+		}
+	}
 
+}
