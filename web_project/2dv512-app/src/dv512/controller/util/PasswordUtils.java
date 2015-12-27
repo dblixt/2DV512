b92b044df10271a363e820ca3e746cf9bcc17472
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2015-12-27 18:27:32 +0100

    password hashing implemented
    users table have a new structure.

---
+++ web_project/2dv512-app/src/dv512/controller/util/PasswordUtils.java
@@ -0,0 +1,64 @@
+
+package dv512.controller.util;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class PasswordUtils {
+
+	private static final Random RND = new SecureRandom();
+	private static final int ITERATIONS = 10000;
+	private static final int KEY_LENGTH = 256; //bits
+
+	/**
+	 * Get next random salt of length 64.
+	 * @return
+	 */
+	public static byte[] getNextSalt() {
+		byte salt[] = new byte[64];
+		RND.nextBytes(salt);
+		return salt;
+	}
+
+	/**
+	 * Generate hash from password and salt.
+	 * @param password
+	 * @param salt
+	 * @return
+	 */
+	public static byte[] hash(char[] password, byte[] salt) {
+		PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
+		Arrays.fill(password, Character.MIN_VALUE);
+		try {
+			SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			return skf.generateSecret(spec).getEncoded();
+		} 
+		catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+			throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+		} 
+		finally {
+			spec.clearPassword();
+		}
+	}
+
+	public static boolean check(char[] password, byte[] salt, byte[] expectedHash) {
+		byte[] pwdHash = hash(password, salt);
+		Arrays.fill(password, Character.MIN_VALUE);
+		if (pwdHash.length != expectedHash.length)
+			return false;
+
+		for (int i = 0; i < pwdHash.length; i++) {
+			if (pwdHash[i] != expectedHash[i])
+				return false;
+		}
+
+		return true;
+	}
+
+}
