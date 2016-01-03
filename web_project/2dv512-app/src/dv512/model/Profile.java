8269a02dcdfac9f2d48a7973e3c9b9b9007dfd3e
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2016-01-03 13:24:35 +0100

    feed radius + input validation on edit profile/dog

--- web_project/2dv512-app/src/dv512/model/Profile.java
+++ web_project/2dv512-app/src/dv512/model/Profile.java
@@ -12,6 +12,7 @@ public class Profile {
 
 	private double longitude;
 	private double latitude;
+	private int radius;
 
 	private String image;
 
@@ -68,7 +69,15 @@ public class Profile {
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
+	
+	public int getRadius() {
+		return radius;
+	}
 
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
 	public String getImage() {
 		return image;
 	}
