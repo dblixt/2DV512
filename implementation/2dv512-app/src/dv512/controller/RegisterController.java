dc570272efb4eea6b8148c1d49f12e4cdaa3cdf7
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2016-01-08 15:43:25 +0100

    fix page titles

--- implementation/2dv512-app/src/dv512/controller/RegisterController.java
+++ implementation/2dv512-app/src/dv512/controller/RegisterController.java
@@ -41,12 +41,10 @@ public class RegisterController implements Serializable {
 	}
 
 	public int getMode() {
-		System.out.println("Mode is: " + mode);
 		return mode;
 	}
 
 	public void switchMode(AjaxBehaviorEvent event) {
-		System.out.println("Switching Mode");
 		if (mode == DEFAULT_MODE) {
 			mode = REGISTER_MODE;
 			return;
